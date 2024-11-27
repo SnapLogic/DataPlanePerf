70d711709da572fca01a3c353b39c516ebe615ed
Anton Samsonov <asamsonov@snaplogic.com>
2024-11-27 08:50:30 -0800

    refactored repo

--- perf/jRunner.py
+++ perf/jRunner.py
@@ -3,8 +3,23 @@ import subprocess
 import argparse
 from datetime import datetime
 
-def run_jmeter(jmx_file, threads, ramp_up, loop_count, report_file, base_url, jmeter_path="/opt/apache-jmeter/bin/jmeter.sh"):
-    # Remove report file if it already exists
+
+# args: jmx_file, root_dir
+def search_jmx(jmx_file_name, root_dir='/'):
+    print(f"Searching for given file: {jmx_file_name}")
+    matching_file = []
+    current_directory = os.getcwd()
+    search_dirs = [current_directory] if root_dir == '/' else [current_directory, root_dir]
+    for base_dir in search_dirs:
+        for walk_dir, dir_names, file_names in os.walk(base_dir):
+            for name in file_names:
+                if jmx_file_name.lower() in name.lower():
+                    matching_file.append(os.path.join(walk_dir, name))
+    return matching_file
+
+
+def run_jmeter(file_path, threads, ramp_up, loop_count, report_file, base_url, jmeter_path="jmeter"):
+    # Remove test report file if it already exists
     if os.path.exists(report_file):
         os.remove(report_file)
         print(f"Existing report file '{report_file}' removed.")
@@ -19,7 +34,7 @@ def run_jmeter(jmx_file, threads, ramp_up, loop_count, report_file, base_url, jm
     command = [
         jmeter_path,
         "-n",                                 # Run in non-GUI mode
-        "-t", jmx_file,                       # JMX test file
+        "-t", file_path,                      # JMX test file
         "-JTHREADS={}".format(threads),       # Number of threads
         "-Jrampup={}".format(ramp_up),        # Ramp-up period
         "-Jloopcount={}".format(loop_count),  # Loop count
@@ -42,6 +57,7 @@ def run_jmeter(jmx_file, threads, ramp_up, loop_count, report_file, base_url, jm
         print(e.stderr.decode())
         return None
 
+
 if __name__ == "__main__":
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Run JMeter test plans with specified parameters.")
@@ -50,21 +66,37 @@ if __name__ == "__main__":
     parser.add_argument("base_url", help="Base URL for the environment under test (positional argument)")
     parser.add_argument("-r", "--rampup", type=int, default=1, help="Ramp-up time in seconds (default: 1)")
     parser.add_argument("-l", "--loopcount", type=int, default=1, help="Loop count (default: 1)")
-    parser.add_argument("-o", "--output", help="Path to the output results file (optional, defaults to a timestamped filename)")
-    parser.add_argument("-p", "--jmeterpath", default="/opt/apache-jmeter/bin/jmeter.sh", help="Path to the JMeter executable")
+    parser.add_argument("-o", "--output",
+                        help="Path to the output results file (optional, defaults to a timestamped filename)")
+    parser.add_argument("-p", "--jmeterpath", default="jmeter", help="Path to the JMeter executable")
 
     args = parser.parse_args()
+
+    # If a file path was provided in args.file, just use it.
+    jmx_file = args.file
+
+    # If no file is found, attempt to search for it in the specified directory
+    if not os.path.exists(jmx_file):
+        print(f"File '{jmx_file}' not found in the specified path. Attempting to search...")
+        matching_files = search_jmx(jmx_file, '/data-plane')  # Adjust search root if needed
+
+        if not matching_files:
+            print("File not found in the search path.")
+            exit(1)
+        else:
+            print(f"File found at: {matching_files[0]}")
+            jmx_file = matching_files[0]
 
     # Generate csv report
     if not args.output:
         timestamp = datetime.now().strftime("%b-%d-%I-%M%p").lower()
-        args.output = f"{timestamp}-pst-{args.threads}-threads.csv"
+        args.output = f"results/{timestamp}-pst-{args.threads}-threads.csv"
 
     print(f"Performance results file: {args.output}")
 
     # Run JMeter with parsed arguments
     run_jmeter(
-        jmx_file=args.file,
+        file_path=jmx_file,
         threads=args.threads,
         ramp_up=args.rampup,
         loop_count=args.loopcount,
